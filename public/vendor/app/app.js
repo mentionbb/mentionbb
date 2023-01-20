3d100088f394ecaac3baed396b4613d2b9007de5
ParantezTech <parantezprojects@gmail.com>
2023-01-20 15:12:42 +0300

    TinyMCE design has been changed

--- public/vendor/app/app.js
+++ public/vendor/app/app.js
@@ -60,7 +60,10 @@ var app = {};
             skin: "oxide",
 
             skin_url: app.config.settings.public_dir + "/vendor/tinymce/skins/ui/" + theme,
-            content_css: app.config.settings.public_dir + "/vendor/tinymce/skins/content/" + content_theme + "/content.min.css",
+            content_css: [
+                app.config.settings.public_dir + "/vendor/tinymce/skins/content/" + content_theme + "/content.min.css",
+                app.config.settings.public_dir + "/themes/frontend/assets/editor/content.css"
+            ],
 
             content_style: ".mce-content-body[data-mce-placeholder]:not(.mce-visualblocks)::before { color: rgb(150 150 150); } p {margin:0; padding: 0;} img.js-Twemoji_MentionEditor { width: 22px; height: 22px; vertical-align: text-bottom; } .mce-content-body [data-mce-selected=inline-boundary] { color: #000; }",
 
