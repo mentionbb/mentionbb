d5d4bbb9b339189e52154a08c1615f2159b392f0
ParantezTech <parantezprojects@gmail.com>
2023-01-21 17:22:17 +0300

    Fixed the line problem in cases such as show source, preview

--- public/vendor/app/editor/bbcode.js
+++ public/vendor/app/editor/bbcode.js
@@ -46,7 +46,7 @@ if (window.jQuery === undefined) jQuery = $ = {};
 						$.each(app.setupEditorPostProcess(), function (idx2, val2) {
 							(o(new RegExp(`${idx2}`, 'gi'), val2), t)
 						}),
-						o(/\n/gi, '<br />'),
+						o(/^\n$/gi, '<br />'),
 						t
 					);
 				})(o.content));
