3f90888a880e6e3ad65c6cee99621514250d54c3
ParantezTech <parantezprojects@gmail.com>
2023-01-22 18:27:50 +0300

    Added defs file

---
+++ bin/defs.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * 
+ * !! Mention is a paid software. !!
+ * !! The maker of this software: ParantezTech | https://www.r10.net/members/90047-scarecrow.html
+ * !! No used Null. !!
+ * !! As long as you are using a Null version, you will not be able to receive updates. !!
+ *
+ * !! The software uses ParFramework2, this infrastructure cannot be used by third parties or any other project can not be produced. !!
+ * 
+ */
+
+define('INDEX_DIR', __DIR__ . '/../');
+
+//Application location.
+define('APPLICATION_SELF', INDEX_DIR . '/src');
+
+//Config Dir
+define('CONFIG_DIR', APPLICATION_SELF . '/App/Config');
+
+//Backup Dir
+define('BACKUP_DIR', APPLICATION_SELF . '/Backups');
+
+//Cache Dir
+define('CACHE_DIR', APPLICATION_SELF . '/Cache');
+
+//This "Public" route.
+define('PUBLIC_DIR', 'public');
+
+//Timing
+define('TIMER_START', microtime(true));
--- index.php
+++ index.php
@@ -13,25 +13,8 @@
 
 ini_set('max_execution_time', 900);
 
-define('INDEX_DIR', __DIR__);
-
-//Application location.
-define('APPLICATION_SELF', INDEX_DIR . '/src');
-
-//Config Dir
-define('CONFIG_DIR', APPLICATION_SELF . '/App/Config');
-
-//Backup Dir
-define('BACKUP_DIR', APPLICATION_SELF . '/Backups');
-
-//Cache Dir
-define('CACHE_DIR', APPLICATION_SELF . '/Cache');
-
-//This "Public" route.
-define('PUBLIC_DIR', 'public');
-
-//Timing
-define('TIMER_START', microtime(true));
+$dir = __DIR__;
+require($dir . '/bin/defs.php');
 
 /**
  * Run Application
