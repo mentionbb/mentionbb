6432cdc0e4f39e9564c301f357dde1696c0137dd
ParantezTech <parantezprojects@gmail.com>
2023-02-16 20:15:44 +0300

    Remove test codes

--- src/App/Mvc/Pub/Test.php
+++ src/App/Mvc/Pub/Test.php
@@ -11,34 +11,6 @@ class Test extends Controller implements Pub
     {
         if ($this->request->getRequestMethod() == "GET")
         {
-            $xml = new \App\Renderer\Response\Xml($this->request);
-            $xml->init();
-
-            $xml->serialize([
-                'foo' => [
-                    'attributes' => [
-                        'bar' => 'foo'
-                    ],
-                    'content' => [
-                        'ss' => [
-                            'attributes' => [
-                                'id' => 1,
-                                'id2' => 2
-                            ],
-                            'content' => [
-                                'sub' => [
-                                    'content' => 'sub'
-                                ],
-                                'sssa' => [
-                                    'content' => '&'
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]);
-
-            $xml->render();
         }
         elseif ($this->request->getRequestMethod() == "POST")
         {
