791f27e8f04d9fa6a7ea24e81da2c7100f488f07
ParantezTech <parantezprojects@gmail.com>
2024-01-17 17:56:20 +0300

    PHP 8 improvements

--- index.php
+++ index.php
@@ -15,24 +15,30 @@
 
 ini_set('max_execution_time', 900);
 
-$dir = __DIR__;
-require($dir . '/bin/defs.php');
-
-if (!file_exists(FRAMEWORK_LIBS . '/vendor/autoload.php'))
+return (function (): \App\Mvc\Pub|bool
 {
-    require($dir . '/bin/RuntimeErrors.php');
-    RuntimeErrors::Show('vendor');
+    $dir = __DIR__;
+    require($dir . '/bin/defs.php');
 
-    return;
-}
+    if (!file_exists(FRAMEWORK_LIBS . '/vendor/autoload.php'))
+    {
+        require($dir . '/bin/RuntimeErrors.php');
+        RuntimeErrors::Show('vendor');
 
-/**
- * Run Application
- */
-require(FRAMEWORK_LIBS . '/vendor/autoload.php');
-new \App\Mvc\Pub();
+        return false;
+    }
 
-/**
- * Logger
- */
-\App\Logger::ErrorLogger();
+    require(FRAMEWORK_LIBS . '/vendor/autoload.php');
+
+    /**
+     * Run Application
+     */
+    $app = new \App\Mvc\Pub();
+
+    /**
+     * Logger
+     */
+    \App\Logger::ErrorLogger();
+
+    return $app;
+})();
