b095cd3d40572a4f4774ca04a29b72d831c06d6a
ParantezTech <parantezprojects@gmail.com>
2024-01-17 14:34:45 +0300

    Remove copyright comments

--- bin/defs.php
+++ bin/defs.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * 
- * !! Mention is a paid software. !!
- * !! The maker of this software: ParantezTech | https://www.r10.net/members/90047-scarecrow.html
- * !! No used Null. !!
- * !! As long as you are using a Null version, you will not be able to receive updates. !!
- *
- * !! The software uses ParFramework2, this infrastructure cannot be used by third parties or any other project can not be produced. !!
- * 
- */
-
 define('INDEX_DIR', __DIR__ . '/..');
 
 //Framework library.
