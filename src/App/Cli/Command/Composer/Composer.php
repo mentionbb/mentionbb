e4dfed0fe4bc2019de2ac0304eecc72587bf7d68
ParantezTech <parantezprojects@gmail.com>
2024-02-01 00:41:17 +0300

    Added dump-autoload command and fix InputArguments

--- src/App/Cli/Command/Composer/Composer.php
+++ src/App/Cli/Command/Composer/Composer.php
@@ -12,18 +12,18 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-class Update extends Command
+class Composer extends Command
 {
     protected function configure(): void
     {
         $this->setName(\App\Cli\CommandList::COMPOSER)
-            ->setDescription('This command allows you to easily perform the composer update.')
-            ->addArgument('update', InputArgument::OPTIONAL);
+            ->setDescription('This command allows you to easily perform the composer.')
+            ->addArgument('composer_command', InputArgument::REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        if ($input->getArgument('update'))
+        if ($input->getArgument('composer_command') == 'update')
         {
             $output = new loggableOutput($this->getName(), $output);
 
@@ -41,9 +41,26 @@ class Update extends Command
             {
                 $output->writeln("<fg=white;bg=green;options=bold>{$buffer}</>");
             });
-        }
 
-        $output->log();
+            $output->log();
+        }
+        else if ($input->getArgument('composer_command') == 'dump-autoload')
+        {
+            $output->writeln('<info>Starting composer dump autoloading..</info>');
+
+            $process = new Process(['php', FRAMEWORK_LIBS . '/bin/composer.phar', 'dump-autoload', '-o']);
+            $process->run();
+
+            if (!$process->isSuccessful())
+            {
+                throw new ProcessFailedException($process);
+            }
+
+            $process->run(function ($type, $buffer) use ($output)
+            {
+                $output->writeln("<fg=white;bg=green;options=bold>{$buffer}</>");
+            });
+        }
 
         return Command::SUCCESS;
     }
