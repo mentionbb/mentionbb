eb7402a4ae3c9bf41a266d5a5ad8d6cdee593a07
ParantezTech <parantezprojects@gmail.com>
2024-04-05 02:07:19 +0300

    Compatible with the new dbal version

--- src/App/Renderer/BBCode/Str/Emoticons.php
+++ src/App/Renderer/BBCode/Str/Emoticons.php
@@ -9,117 +9,118 @@ use JoyPixels\Ruleset;
 
 class Emoticons
 {
-	private $client;
-	private $settings;
+    private $client;
+    private $settings;
 
-	public function __construct()
-	{
-		$this->client = new Client($this->getRuleset());
-		$this->client->ascii = true;
+    public function __construct()
+    {
+        $this->client = new Client($this->getRuleset());
+        $this->client->ascii = true;
 
-		$this->settings = (new \App\Entity\Settings())->getSettings();
-	}
+        $this->settings = (new \App\Entity\Settings())->getSettings();
+    }
 
-	public function Render($string)
-	{
-		$string = $this->asciiToUnicode($string);
+    public function Render($string)
+    {
+        $string = $this->asciiToUnicode($string);
 
-		$source = preg_replace_callback('/' . $this->client->ignoredRegexp . '|' . $this->client->unicodeRegexp . '/u', function ($matches)
-		{
-			if (!is_array($matches) || !isset($matches[0]) || empty($matches[0]))
-			{
-				return $matches[0];
-			}
+        $source = preg_replace_callback('/' . $this->client->ignoredRegexp . '|' . $this->client->unicodeRegexp . '/u', function ($matches)
+        {
+            if (!is_array($matches) || !isset($matches[0]) || empty($matches[0]))
+            {
+                return $matches[0];
+            }
 
-			$ruleset = $this->getRuleset();
-			$unicodeReplace = $ruleset->getUnicodeReplace();
+            $ruleset = $this->getRuleset();
+            $unicodeReplace = \array_flip($ruleset->getUnicodeReplace());
 
-			$unicode = $matches[0];
+            $unicode = $matches[0];
 
-			if (array_key_exists($unicode, $unicodeReplace))
-			{
-				$shortname = $unicodeReplace[$unicode];
-			}
-			else
-			{
-				return $matches[0];
-			}
+            if (array_key_exists($unicode, $unicodeReplace))
+            {
+                $shortname = $unicodeReplace[$unicode];
+            }
+            else
+            {
+                return $matches[0];
+            }
 
-			return $this->getImageFromShortname($shortname);
-		},  $string);
+            return $this->getImageFromShortname($shortname);
+        },  $string);
 
-		return $source;
-	}
+        return $source;
+    }
 
-	public function getImageFromShortname($string)
-	{
-		$source = preg_replace_callback('/' . $this->client->ignoredRegexp . '|' . $this->client->shortcodeRegexp . '/Si', function ($matches)
-		{
-			if (!is_array($matches) || !isset($matches[0]) || empty($matches[0]))
-			{
-				return $matches[0];
-			}
-			else
-			{
-				$ruleset = $this->getRuleset();
-				$shortcodeReplace = $ruleset->getShortcodeReplace();
+    public function getImageFromShortname($string)
+    {
+        $source = preg_replace_callback('/' . $this->client->ignoredRegexp . '|' . $this->client->shortcodeRegexp . '/Si', function ($matches)
+        {
+            if (!is_array($matches) || !isset($matches[0]) || empty($matches[0]))
+            {
+                return $matches[0];
+            }
+            else
+            {
+                $ruleset = $this->getRuleset();
+                $shortcodeReplace = $ruleset->getShortcodeReplace();
 
-				$shortname = $matches[0];
+                $shortname = $matches[0];
 
-				if (!isset($shortcodeReplace[$shortname]))
-				{
-					return $matches[0];
-				}
+                if (!isset($shortcodeReplace[$shortname]))
+                {
+                    return $matches[0];
+                }
 
-				$alt = htmlspecialchars($this->client->shortnameToUnicode($shortname));
+                $alt = htmlspecialchars($this->client->shortnameToUnicode($shortname));
 
-				$image = $shortcodeReplace[$shortname][0];
-				$image = $this->replaceFe0f($image);
+                $shortcode = $shortcodeReplace[$shortname];
+                $image = $this->replaceFe0f($shortcode[0]);
+                $parent = $shortcode[2];
 
-				$database = sprintf($this->getEmoticonDatabase(), $image);
+                $database = sprintf($this->getEmoticonDatabase(), $image);
 
-				$title = str_replace(['_', ':'], [' ', ''], $shortname);
-				$title = ucfirst($title);
+                $title = str_replace(['_', ':'], [' ', ''], $shortname);
+                $title = ucfirst($title);
 
-				return "<img class=\"js-Twemoji_MentionEditor\" src=\"{$database}\" alt=\"{$alt}\" data-shortcut=\"{$shortname}\" data-title=\"{$title}\" />&nbsp;";
-			}
-		}, $string);
+                return "<img class=\"js-Twemoji_MentionEditor\" src=\"{$database}\" alt=\"{$alt}\" data-shortcut=\"{$shortname}\" data-title=\"{$title}\" />&nbsp;";
+            }
+        }, $string);
 
-		return $source;
-	}
+        return $source;
+    }
 
-	public function asciiToUnicode($string)
-	{
-		$string = $this->client->asciiToShortname($string);
-		$string = $this->client->shortnameToUnicode($string);
+    public function asciiToUnicode($string)
+    {
+        $string = $this->client->asciiToShortname($string);
+        $string = $this->client->shortnameToUnicode($string);
 
-		return $string;
-	}
+        return $string;
+    }
 
-	protected function replaceFe0f($filename)
-	{
-		if (preg_match('/^([a-z0-9]+-fe0f)/', $filename, $fe0fMatch))
-		{
-			return str_replace('-fe0f', '', $fe0fMatch[0]);
-		}
+    protected function replaceFe0f($filename)
+    {
+        if (preg_match('/^([a-z0-9]+-fe0f)/', $filename, $fe0fMatch))
+        {
+            return str_replace('-fe0f', '', $fe0fMatch[0]);
+        }
 
-		return $filename;
-	}
+        return $filename;
+    }
 
-	protected function getRuleset()
-	{
-		return new Ruleset();
-	}
+    protected function getRuleset()
+    {
+        return new Ruleset();
+    }
 
-	private function getEmoticonDatabase()
-	{
-		if ($this->settings->emoticon_database == 'local')
-		{
-			return AppSub::getPublicDir() . "/images/emoticons/%s.png";
-		}
-		else if ($this->settings->emoticon_database == 'twemoji')
-		{
-			return "https://twemoji.maxcdn.com/v/latest/72x72/%s.png";
-		}
-	}
+    private function getEmoticonDatabase()
+    {
+        if ($this->settings->emoticon_database == 'local')
+        {
+            return AppSub::getPublicDir() . "/images/emoticons/%s.png";
+        }
+        else if ($this->settings->emoticon_database == 'twemoji')
+        {
+            return "https://twemoji.maxcdn.com/v/latest/72x72/%s.png";
+        }
+    }
 }
