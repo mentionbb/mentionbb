fa177b4975cbcbc87668b80dfe0aa328e3d55936
ParantezTech <parantezprojects@gmail.com>
2024-04-07 17:22:19 +0300

    Fix specification mandate value for attribute

--- src/App/Hook/Filter/FilterTag.php
+++ src/App/Hook/Filter/FilterTag.php
@@ -4,10 +4,20 @@ namespace App\Hook\Filter;
 
 class FilterTag
 {
-    public static function filterSingleTags($content)
+    /**
+     * filterSingleTags
+     * 
+     * Fix specification mandate value for attribute required.
+     *
+     * @param  mixed $content
+     * @return string
+     */
+    public static function filterSingleTags($content): string
     {
         $content = preg_replace('/<(br|hr)>/si', '<$1 />', $content);
         $content = preg_replace('/<img(.*?)>/si', '<img$1 />', $content);
+
+        $content = preg_replace('/(disabled|required|selected)/si', '$1="$1"', $content);
 
         return $content;
     }
