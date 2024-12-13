e10b406f62000060759a1810ded29cefcc584021
par247 <parantezprojects@gmail.com>
2024-12-13 16:46:57 +0300

    Add new captcha option (Cloudflare Turnstile)

--- src/App/SubContainer/Captcha.php
+++ src/App/SubContainer/Captcha.php
@@ -4,37 +4,79 @@ namespace App\SubContainer;
 
 use App\Phrase;
 use App\Repository\Request;
-use ReCaptcha\ReCaptcha;
+
+use ReCaptcha\ReCaptcha as GoogleReCaptcha;
+
+use Symfony\Component\HttpClient\Psr18Client;
+use Turnstile\Error\Code;
+use Turnstile\Turnstile;
 
 class Captcha
 {
-	public static function isValid($response)
-	{
-		$settings = Phrase::getSettings();
-		$request = (new Request());
+    public static function isValid($response)
+    {
+        $settings = Phrase::getSettings();
+        $request = (new Request());
 
-		if ($request->getServerName() == 'localhost')
-		{
-			$hostname = 'testkey.google.com';
-		}
-		else
-		{
-			$hostname = $request->getServerName();
-		}
+        if ($settings->recaptcha_using == 'google')
+        {
+            if ($request->getServerName() == 'localhost')
+            {
+                $hostname = 'testkey.google.com';
+            }
+            else
+            {
+                $hostname = $request->getServerName();
+            }
 
-		$recaptcha = new ReCaptcha($settings->gr_secret_key);
-		$resp = $recaptcha->setExpectedHostname($hostname)->verify(
-			$response,
-			$request->getRemoteAddr()
-		);
+            $recaptcha = new GoogleReCaptcha($settings->gr_secret_key);
+            $resp = $recaptcha->setExpectedHostname($hostname)->verify(
+                $response,
+                $request->getRemoteAddr()
+            );
 
-		if ($resp->isSuccess())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+            if ($resp->isSuccess())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if ($settings->recaptcha_using == 'cloudflare_turnstile')
+        {
+            if (is_null($response))
+            {
+                return false;
+            }
+            
+            $turnstile = new Turnstile(
+                client: new Psr18Client(),
+                secretKey: $settings->gr_secret_key,
+            );
+
+            $response = $turnstile->verify(
+                $response,
+                $_SERVER['REMOTE_ADDR'],
+            );
+
+            if ($response->success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if ($settings->recaptcha_using == 'none')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
